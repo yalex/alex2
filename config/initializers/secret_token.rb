de263d8ddbe98cb4221e3a4bff4446457c8b11b1
Alex Z <yalex4250@yahoo.com>
2011-10-06 20:52:44 -0400

    my second app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alex2::Application.config.secret_token = '392554d74f97a3e14fc3234704cd064a7abd4bb62389523fa405645b4bb060a9546f65e39a263c75acdabad97b83d1111c5be1509864133423debc900996a0d4'
